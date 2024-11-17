652eb4cd3968ae523ad8528071b4d3e0fc5b7488
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-11-17 19:27:29 +0900

    add new file to generate wat format text

---
+++ src/abs/gen.rs
@@ -0,0 +1,31 @@
+
+enum wasm_instructions
+{
+    // 
+    local_get,
+    local_set,
+    // i32 
+    const_i32,
+    add_i32,
+    // i64
+    const_i64,
+    add_i64,
+    // f32
+    const_f32,
+    // f64
+    const_f64,
+}
+
+
+
+/// wasm target support
+///
+/// この関数では関数の呼びだしに対して
+/// wasmランタイムの命令に従ったwat形式を返却します。
+/// ここでの変換則がlichenのすべてのルールに対応でき
+/// ているわけではないことに注意してください
+pub trait Wasm_gen {
+    fn generate(&self) -> String;
+}
+
+
