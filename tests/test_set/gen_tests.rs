adbb1c54ff684636c6aa075a06350c05c05e4122
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-11-25 23:59:56 +0900

    add wasm simd example

--- tests/test_set/gen_tests.rs
+++ tests/test_set/gen_tests.rs
@@ -102,6 +102,38 @@ pub fn run_wasm() -> anyhow::Result<()> {
 }
 
 
+/// simdの実験
+#[test]
+pub fn run_wasm2() -> anyhow::Result<()> {
+    let module_wat = r#"
+    (module
+    (type $t0 (func (param i32) (result i32)))
+    (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+    (local $a v128)
+    v128.const i32x4 3 2 3 4
+    local.set $a
+    local.get $a
+    i32x4.extract_lane 3
+    ))
+    "#;
+
+    let mut store = Store::default();
+    let module = Module::new(&store, module_wat)?;
+    // The module doesn't import anything, so we create an empty import object.
+    let import_object = imports! {};
+    let instance = Instance::new(&mut store, &module, &import_object)?;
+
+    let add_one = instance.exports.get_function("add_one")?;
+
+    for i in 0..10{
+        let result = add_one.call(&mut store, &[Value::I32(i)])?;
+        println!("i:{} result: {}", i,result[0]);
+        // assert_eq!(result[0], Value::I32(i + 1));
+    }
+    Ok(())
+}
+
+
 #[test]
 pub fn gen_test00(){
     let test_cases = vec![
